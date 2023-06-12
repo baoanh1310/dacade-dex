b3a2eedb324f3c13be5636f7140400af7507f5b4
WalterBrian <133803737+WalterBrian@users.noreply.github.com>
2023-06-12 07:57:09 +0100

    Update Token.sol

--- contracts/contracts/Token.sol
+++ contracts/contracts/Token.sol
@@ -5,14 +5,29 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract IcebearToken is ERC20 {
 
+    /**
+     * @dev Event emitted when the faucet function mints tokens to an address.
+     * @param minter The address of the account that minted the tokens.
+     * @param amount The amount of tokens minted.
+     */
     event Faucet(address indexed minter, uint256 amount);
 
+    /**
+     * @dev Default amount of tokens to be minted by the faucet function.
+     * Modify this value to adjust the default minting amount.
+     */
     uint256 public DEFAULT_MINT_AMOUNT = 1000;
 
+    /**
+     * @dev Contract constructor.
+     * It initializes the IcebearToken contract with the name "Icebear Token" and symbol "ICB".
+     */
     constructor() ERC20("Icebear Token", "ICB") {
     }
 
-    // faucet 1000 ICB tokens
+    /**
+     * @dev Faucet function that mints 1000 ICB tokens to the caller.
+     */
     function faucet() public {
         uint256 amount = DEFAULT_MINT_AMOUNT * (10 ** decimals());
         _mint(msg.sender, amount);
